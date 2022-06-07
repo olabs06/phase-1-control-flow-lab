7a87b31a5f7900266bea6ee6820174a1182281b7
Simeon <olabs06@yahoo.com>
2022-06-07 16:48:53 +0100

    modifying index.js

--- index.js
+++ index.js
@@ -1,11 +1,43 @@
-function scuberGreetingForFeet(){
+function scuberGreetingForFeet(ride){
   // Write your code here!
+  
+  let result;
+
+  if (ride <= 400) {
+      result = 'This one is on me!';
+  } else if (ride >= 2000){
+      result = 'I will gladly take your thirty bucks.';
+  } if (ride >= 2500) {
+      result = 'No can do.';
+  }
+    return result
+
 }
 
-function ternaryCheckCity(){
+function ternaryCheckCity(city){
   // Write your code here!
+
+  const destinationCity = city === "NYC" ? "Ok, sounds good." : "No go.";
+
+  return destinationCity;
 }
 
-function switchOnCharmFromTip(){
+
+function switchOnCharmFromTip(greeting){
   // Write your code here!
+  let response;
+
+  switch (greeting) {
+    case 'generous':
+        response = 'Thank you so much.';
+        break;
+    case 'not as generous':
+        response = 'Thank you.';
+        break;
+    default:
+        response = 'Bye.'
+  }
+
+  return response
 }
+
